e2f81bfdcae4344cb20caa7711863031c9860934
Cristian Justin Gomezcoello Londoño <cristian.gomezcoello@udla.edu.ec>
2025-05-17 16:35:45 -0500

    cambio numero y correo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,8 @@ const Footer: React.FC = () => {
       <div className="container mx-auto px-4 flex flex-col md:flex-row justify-between items-center text-center md:text-left">
         {/* Información de contacto */}
         <div className="text-gray-700 text-sm">
-          <p>Celular: 0978706157</p>
-          <p>Correo: jenny@gmail.com</p>
+          <p>Celular: 0967110198</p>
+          <p>Correo: jhon.sanchez@espoch.edu.ec </p>
         </div>
 
         {/* Información de la institución */}
